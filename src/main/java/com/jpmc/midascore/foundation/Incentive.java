5c102373ccbc0e2b08598f0ba2b808f35b0338c1
Matthew Leong <mattleong43@gmail.com>
2026-01-15 15:01:37 -0500

    Completed Task 5: Expose a REST API in a Spring application

--- src/main/java/com/jpmc/midascore/foundation/Incentive.java
+++ src/main/java/com/jpmc/midascore/foundation/Incentive.java
@@ -3,7 +3,7 @@ package com.jpmc.midascore.foundation;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Incentive { //note: run transaction-incentive-api.jar file before running debug test for task 4
+public class Incentive { //note: run transaction-incentive-api.jar file before running tests for task 4 and 5
 
     private float amount;
 
